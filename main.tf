1ba8066a4047df9727279908cf3a4e342ade8779
Ashok Das <sad.kohsa@gmail.com>
2022-06-10 15:31:06 +0530

    terraform

--- main.tf
+++ main.tf
@@ -3,76 +3,25 @@ credentials = "${file("cr-gcp-348307-152507cf30d0.json")}"
 project = "cr-gcp-348307"
 region = "us-west1"
 }
-resource "google_compute_instance" "nexus" {
+resource "google_compute_instance" "grafana" {
   project      = "cr-gcp-348307"
-  name         = "nexus"
+  name         = "grafana"
   machine_type = "e2-medium"
   zone         = "us-west1-a"
-  tags         = ["ssh", "port-8081"]
+  tags         = ["ssh", "port-9090", "port-3000", "port-8080", "port-9000"]
    boot_disk {
     initialize_params {
-      image = "cr-nexus-image"
+      image = "cr-pro-sonar"
     }
   }
   network_interface {
     network = "default"
-    network_ip = "10.138.0.10"
+    network_ip = "10.138.0.12"
     access_config {
     }
   }
 }
 
-#resource "google_compute_instance" "jenkins" {
-#  project      = "cr-gcp-348307"
-#  name         = "jenkins"
-#  machine_type = "e2-medium"
-#  zone         = "us-west1-a"
-#  tags         = ["ssh", "port-8080"]
-#   boot_disk {
-#    initialize_params {
-#      image = "cr-jenkins"
-#    }
-#  }
-#  network_interface {
-#    network = "default"
-#    access_config {
-#    }
-#  }
-#}
-resource "google_compute_instance" "prometheus" {
-  project      = "cr-gcp-348307"
-  name         = "prometheus"
-  machine_type = "e2-medium"
-  zone         = "us-west1-a"
-  tags         = ["ssh", "port-9090"]
-   boot_disk {
-    initialize_params {
-      image = "cr-prometheus-image"
-    }
-  }
-  network_interface {
-    network = "default"
-    access_config {
-    }
-  }
-}
-resource "google_compute_instance" "sonarqube" {
-  project      = "cr-gcp-348307"
-  name         = "sonarqube"
-  machine_type = "e2-medium"
-  zone         = "us-west1-a"
-  tags         = ["ssh", "port-9000"]
-   boot_disk {
-    initialize_params {
-      image = "cr-sonar-image"
-    }
-  }
-  network_interface {
-    network = "default"
-    access_config {
-    }
-  }
-}
 resource "google_compute_firewall" "allow-tcp" {
   name = "allow-tcp"
   network = "default"
@@ -85,17 +34,17 @@ resource "google_compute_firewall" "allow-tcp" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["ssh"]
 }
-resource "google_compute_firewall" "allow-port8081" {
-  name = "allow-port8081"
+resource "google_compute_firewall" "allow-port3000" {
+  name = "allow-port3000"
   network = "default"
   allow {
     protocol = "tcp"
-    ports    = ["8081"]
+    ports    = ["3000"]
   }
   direction     = "INGRESS"
   priority      = 1000
   source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["port-8081"]
+  target_tags   = ["port-3000"]
 }
 resource "google_compute_firewall" "allow-port8080" {
   name = "allow-port8080"
