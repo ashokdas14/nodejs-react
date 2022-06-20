c032c7fb86b9b037754a88c71381c1f37f4a6115
ashokdas14 <89927673+ashokdas14@users.noreply.github.com>
2022-06-20 20:08:08 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,8 +1,8 @@
-#const sonarqubeScanner = require('sonarqube-scanner');
- #    sonarqubeScanner({
-  #     serverUrl: 'http://10.138.0.12:9000',
-   #    options : {
-    #   'sonar.sources': '.',
-     #  'sonar.inclusions' : 'src/**' // Entry point of your code
-      # }
-    # }, () => {});
+const sonarqubeScanner = require('sonarqube-scanner');
+    sonarqubeScanner({
+      serverUrl: 'http://10.128.0.3:9000',
+      options : {
+       'sonar.sources': '.',
+       'sonar.inclusions' : 'src/**' // Entry point of your code
+       }
+     }, () => {});
